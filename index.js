6ce490b82549fd44cddfd06cc0db953926e63f42
Mohammed Bakr <mohammed.mobile.tricks@gmail.com>
2020-05-20 16:10:09 +0200

    Update index.js

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const client = new Discord.Client();
 const http = require('http');
 const express = require('express');
 const app = express();
+const modColor = '#ee3333';
 app.get("/", (request, response) => {
   console.log(Date.now() + " Ping Received");
   response.sendStatus(200);
@@ -39,7 +40,7 @@ client.on("message", message => {
     }
 
     if(!message.author.bot) {
-        if(message.content.startsWith(`${prefix}ping`)) {
+        if(message.content.toLowerCase().startsWith(`${prefix}ping`)) {
             message.channel.send("Pong");
         }
         if(message.content.startsWith(`${prefix}help`)) {
@@ -47,14 +48,14 @@ client.on("message", message => {
         }
     }
   
-      if(message.content.startsWith(`${prefix}admit`)) {
+      if(message.content.toLowerCase().startsWith(`${prefix}admit`)) {
         let toAdmit = message.mentions.members.first();
         let member = message.guild.roles.find(role => role.name === 'Member');
         let newMember = message.guild.roles.find(role => role.name === 'NEW-MEMBER');
         let admitted = new Discord.RichEmbed();
         admitted.setAuthor(message.author.username, toAdmit.displayAvatarURL).
-        setColor('RANDOM').
-        setDescription(`${toAdmit} was admitted by ${message.author.username}`)
+        setColor(modColor).
+        setDescription(`${toAdmit} was admitted by ${message.author}`)
         .setImage(toAdmit.avatarURL);
         if(message.member.roles.some(role => role.name === 'Minor Staff Permissions')
           || message.member.roles.some(role => role.name === 'Staff-Ping')) {
@@ -65,6 +66,56 @@ client.on("message", message => {
             });
             setTimeout(() => {
               client.channels.get('668991257170935818').send(admitted)
+            }, 4000);
+            
+        }
+    }
+  
+  // Detention
+  
+  if(message.content.toLowerCase().startsWith(`${prefix}det`)) {
+        let toDetention = message.mentions.members.first();
+        let member = message.guild.roles.find(role => role.name === 'Member');
+        let detention = message.guild.roles.find(role => role.name === 'Detention');
+        let detentioned = new Discord.RichEmbed();
+        detentioned.setAuthor(message.author.username, toDetention.displayAvatarURL).
+        setColor(modColor).
+        setDescription(`${toDetention} was sent to Detention by ${message.author}`)
+        .setImage(toDetention.avatarURL);
+        if(message.member.roles.some(role => role.name === 'Minor Staff Permissions')
+          || message.member.roles.some(role => role.name === 'Staff-Ping')) {
+            toDetention.addRole(detention);
+            toDetention.removeRole(member)
+            .catch(() => {
+              message.channel.send("Something went wrong");
+            });
+            setTimeout(() => {
+              client.channels.get('668991257170935818').send(detentioned)
+            }, 4000);
+            
+        }
+    }
+  
+  // Remove Detention
+  
+    if(message.content.toLowerCase().startsWith(`${prefix}rdet`)) {
+        let toDetention = message.mentions.members.first();
+        let member = message.guild.roles.find(role => role.name === 'Member');
+        let detention = message.guild.roles.find(role => role.name === 'Detention');
+        let detentioned = new Discord.RichEmbed();
+        detentioned.setAuthor(message.author.username, toDetention.displayAvatarURL).
+        setColor(modColor).
+        setDescription(`${toDetention} was removed from detention by ${message.author}`)
+        .setImage(toDetention.avatarURL);
+        if(message.member.roles.some(role => role.name === 'Minor Staff Permissions')
+          || message.member.roles.some(role => role.name === 'Staff-Ping')) {
+            toDetention.removeRole(detention);
+            toDetention.addRole(member)
+            .catch(() => {
+              message.channel.send("Something went wrong");
+            });
+            setTimeout(() => {
+              client.channels.get('668991257170935818').send(detentioned)
             }, 4000);
             
         }
